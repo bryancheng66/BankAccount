e29871da2ba92bb047705947afb007eafdb8f458
bryancheng66 <bryancheng66@gmail.com>
2020-10-28 16:34:22 -0400

    Added constructor

---
+++ BankAccount.java
@@ -0,0 +1,11 @@
+public class BankAccount {
+	private double balance;
+	private int accountID;
+	private String password;
+
+	public BankAccount(int accountID, String password){
+		this.balance = 0;
+		this.accountID = accountID;
+		this.password = password;
+	}
+}
